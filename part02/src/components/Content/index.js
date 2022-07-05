3225565a975ec8a83a04be2fb872e99adbe8316f
leandrotedini <leandro.tedini@gmail.com>
2022-07-04 23:34:44 -0300

    Part 02 - Exercise 2.2

--- part02/src/components/Content/index.js
+++ part02/src/components/Content/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import Part from "../Part";
 
 const Content = ({ parts }) => {
+
+  const totalExercises = parts.reduce(
+    (previousValue, part) => {
+      return previousValue += part.exercises
+    }, 0);
+
   return (
-    <ul>
-      {parts.map(part => 
-        <li key={part.id}><Part name={part.name} exercises={part.exercises} /></li>
-      )}
-    </ul>
+    <>
+      <ul>
+        {parts.map(part => 
+          <li key={part.id}><Part name={part.name} exercises={part.exercises} /></li>
+        )}
+      </ul>
+
+      <strong>Total of {totalExercises} exercises</strong>
+    </>
   )
 }
 
