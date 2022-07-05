40f3c165b29038d40656cd7ba7c261b9b6f0a8d5
leandrotedini <leandro.tedini@gmail.com>
2022-07-05 15:11:50 -0300

    Part 02 - Exercise 2.6 and 2.7

---
+++ part02/src/components/TelephoneGuide/index.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+
+
+const TelephoneGuide = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    if (persons.some(item => item.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+    }else{
+      setPersons([...persons, {name: newName}])
+      setNewName('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addName}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default TelephoneGuide
