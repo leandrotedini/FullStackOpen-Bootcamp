7ec6b332dd0a5f9ca46112143e6baa94894edc15
leandrotedini <leandro.tedini@gmail.com>
2022-07-07 17:04:18 -0300

    Part 02 - Exercise 2.7 and 2.9

---
+++ part02/src/components/Person/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Person ({ name, number }) {
+  return (
+    <span>{name} - {number}</span>
+  )
+}
--- part02/src/components/TelephoneGuide/index.js
+++ part02/src/components/TelephoneGuide/index.js
@@ -1,16 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Person from "../Person";
 
 
 
 const TelephoneGuide = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -18,17 +20,35 @@ const TelephoneGuide = () => {
     if (persons.some(item => item.name === newName)){
       alert(`${newName} is already added to phonebook`)
     }else{
-      setPersons([...persons, {name: newName}])
+      setPersons([...persons, {name: newName, number: newNumber}])
       setNewName('')
+      setNewNumber('')
     }
   }
 
+  const filterPersons = (filter) =>{
+    return persons.filter( item => {
+      return (
+        item.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+      )
+    })
+  }
+
+  const personsToShow = filter === '' ? persons : filterPersons(filter)
+
+
   return (
     <div>
+      
       <h2>Phonebook</h2>
+      filter shown with <input value={filter} onChange={(event) => setFilter(event.target.value)} />
+      <h2>add a new</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
         </div>
         <div>
           <button type="submit" onClick={addName}>add</button>
@@ -36,7 +56,13 @@ const TelephoneGuide = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {personsToShow.map(person => {
+          return(
+            <li key={person.name}>
+              <Person name={person.name} number={person.number}/>
+            </li>
+          )
+          })}
       </ul>
     </div>
   )
